de263b83837beb0a88cc430289d047c6701643b7
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-07-27 15:51:12 -0500

    adding task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,16 +7,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t counter = 0;
-
 	if (!tree)
 		return (0);
-
-	if (tree->left)
-		counter++;
-
-	if (tree->right)
-		counter++;
-
-	return (counter);
+	if (!tree->left && !tree->right)
+		return (1);
+	else
+		return binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right);
 }
