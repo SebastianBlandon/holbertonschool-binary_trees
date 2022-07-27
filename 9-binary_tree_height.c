4d64549cf44e77ca8e440b88d0ebd61c550a1540
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-07-27 11:30:25 -0500

    adding task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,32 @@
 #include "binary_trees.h"
+
+/**
+ * recursive_height - found recursively height of the tree
+ * @tree: binary tree
+ * Return: height of the tree
+ */
+
+size_t recursive_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		/* compute the depth of each subtree */
+		left_height = recursive_height(tree->left);
+		right_height = recursive_height(tree->right);
+
+		/* use the larger one */
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+	}
+}
 /**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
@@ -7,15 +35,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counter = 0;
-
 	if (!tree)
 		return (0);
-	if (tree->left)
-		counter++;
 
-	if (tree->right)
-		counter++;
-
-	return (counter);
+	return (recursive_height(tree) - 1);
 }
