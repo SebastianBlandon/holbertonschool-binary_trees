0b8786386a32850bd8fabd338d55f416387580b3
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-07-27 16:23:41 -0500

    adding task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - count no leaf nodes
+ * @tree: binary tree
+ * Return: count of no leaf nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
